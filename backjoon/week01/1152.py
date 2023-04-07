a953ece575398734fc86d52bc7357c2f21d34d26
heondeam <aoao1023@gmail.com>
2023-04-07 18:38:39 +0900

    1152 resolved

--- backjoon/week01/1152.py
+++ backjoon/week01/1152.py
@@ -12,6 +12,10 @@ s = open("input.txt", "rt")
 
 """
 
-input_text = [a for a in s.readline().strip().split(" ")]
+# split()과 split(" ")의 차이에 대해서 이해
+# split()은 공백이 1개이건 2개이건 n개이건 상관없이 무조건 1개로 보고 처리
+# split(" ")은 공백 1개, 1개를 각각의 공백으로 따로 따로 처리
+
+input_text = s.readline().strip().split()
 
 print(len(input_text))
