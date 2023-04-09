5e933f106a6a3dcb6a367791c252d9b864d9fbb8
heondeam <aoao1023@gmail.com>
2023-04-09 17:35:58 +0900

    9498 시험 성적 resolved

--- backjoon/week01/9498.py
+++ backjoon/week01/9498.py
@@ -10,14 +10,17 @@ n = int(s.readline())
 시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
 
 """
+def test_score(num):
+    if num <= 100 and num >= 90:
+        print("A")
+    elif num < 90 and num >= 80:
+        print("B")
+    elif num < 80 and num >= 70:
+        print("C")
+    elif num < 70 and num >= 60:
+        print("D")
+    elif num < 60:
+        print("F")
+    
 
-if n <= 100 and n >= 90:
-    print("A")
-elif n < 90 and n >= 80:
-    print("B")
-elif n < 80 and n >= 70:
-    print("C")
-elif n < 70 and n >= 60:
-    print("D")
-elif n < 60:
-    print("F")
+test_score(n)
