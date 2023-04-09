0bd9a2f18b1c69bba1d1d00cc1447f48a376821c
heondeam <aoao1023@gmail.com>
2023-04-09 12:36:21 +0900

    삽입 정렬 알고리즘 구현

--- study/sort.py
+++ study/sort.py
@@ -51,3 +51,22 @@ def selection_sort(list):
     return list
 
 print(selection_sort([4, 2, 1, 7, 3]))
+
+
+# 삽입정렬 구현
+def insertion_sort(array):
+    for index in range(1, len(array)):
+
+        temp_value = array[index]
+        position = index - 1
+    
+        while position >= 0:
+            if array[position] > temp_value:
+                array[position + 1] = array[position]
+                position = position - 1
+            else:
+                break
+        
+        array[position + 1] = temp_value
+
+    return array
