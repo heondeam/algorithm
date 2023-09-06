feccf77025ed6e6e3738c98e1fef1ec236d1a355
heondeam <aoao1023@gmail.com>
2023-09-06 22:08:04 +0900

    프로그래머스 광물캐기 resolved

--- programmers/week02/광물_캐기.js
+++ programmers/week02/광물_캐기.js
@@ -1,39 +1,76 @@
 function solution (picks , minerals) {
-    var answer = 0;
-    let len = Math.ceil(minerals.length / 5);
-    let maxLen = picks.reduce((a, b) => a + b);
-    let arr = [];
-    if (maxLen === 0) return 0;
-    minerals = minerals.splice(0, maxLen * 5);
-
-    for (let a = 0; a < len; a++) {
-      let obj = { d: 0, i: 0, s: 0 };
-      // minerals의 0번째부터 5번째까지 자른후 배열을 순회하면서
-      // 자른 배열의 해당 요소를 obj에 카운터
-      minerals.splice(0, 5).map((v) => obj[v[0]]++);
-      // 각 곡괭이로 5개의 광석을 캤을때 사용되는 피로도를 arr에 추가
-      // 순서대로 [ dia, iron, stone ]
-      // a ~ a + 4 구간에 각 곡괭이의 피로도를 저장 
-      arr.push([
-        obj.d + obj.i + obj.s,
-        obj.d * 5 + obj.i + obj.s,
-        obj.d * 25 + obj.i * 5 + obj.s,
-      ]);
+  let answer = 0;
+  // 누적된 피로도
+  let maxLen = Math.min((picks[0] * 5) + (picks[1] * 5) + (picks[2] * 5), minerals.length);
+  let nowLen = 0;
+  
+  // 곡괭이 수에 맞춰 캘 수 있는 광물.
+  let splitedMinerals = [];
+  
+  // O(N) - minerals.length <= 50
+  while (maxLen > nowLen) {
+    let cnt = maxLen - nowLen;
+    
+    if (cnt > 0 && cnt <= 5) {
+      const nm = minerals.splice(0, cnt);
+      splitedMinerals.push(nm);
+      nowLen += nm.length;
+    }else if(cnt > 5) {
+      const nm = minerals.splice(0, 5);
+      splitedMinerals.push(nm);
+      nowLen += 5;
+    }else {
+      break;
     }
-    // stone 곡괭이로 캤을 때 가장 큰값이 앞에 오도록 내림차순으로 정렬 후
-    // 배열을 순회 하면서 stone 곡괭이로 캤을 때 가장 큰값은 다이아가 많다는 뜻이므로
-    // 다이아 곡괭이(picks[0])를 먼저 사용
-    // 다이아 곡괭이가 없다면 철 곡괭이(picks[1])를 사용
-    // 철 곡괭이도 없다면 돌 곡괭이 사용
-    arr
-      .sort((a, b) => b[2] - a[2])
-      .map((v) => {
-        if (picks[0] > 0) return picks[0]--, (answer += v[0]);
-        if (picks[1] > 0) return picks[1]--, (answer += v[1]);
-        if (picks[2] > 0) return picks[2]--, (answer += v[2]);
-      });
+  }
+  
+  splitedMinerals.sort((a, b) => {
+    let sumA = 0;
+    let sumB = 0;
+    
+    a.forEach(item => item === "diamond" ? sumA += 25 : item === "iron" ? sumA += 5 : sumA += 1);
+    b.forEach(item => item === "diamond" ? sumB += 25 : item === "iron" ? sumB += 5 : sumB += 1);
 
-    return answer;
+    // sumA와 sumB를 비교하여 내림차순으로 정렬
+    if (sumA > sumB) {
+      return -1; // sumA가 크면 b를 앞으로
+    } else if (sumA < sumB) {
+      return 1;  // sumB가 크면 a를 앞으로
+    } else {
+      return 0;  // sumA와 sumB가 같으면 순서 변경 없음
+    }
+  });
+  
+  while (splitedMinerals.length) {
+    const nowMinerals = splitedMinerals.shift();
+    
+    let nowPick;
+    
+    if (picks[0] > 0) {
+      nowPick = "diamond";
+      picks[0] -= 1;
+    }else if (picks[1] > 0) {
+      nowPick = "iron";
+      picks[1] -= 1;
+    }else if (picks[2] > 0) {
+      nowPick = "stone";
+      picks[2] -= 1;
+    }
+    
+    if (nowPick === "diamond") {
+      nowMinerals.forEach(item => answer += 1);
+    }
+    
+    if (nowPick === "iron") {
+      nowMinerals.forEach(item => item === "diamond" ? answer += 5 : item === "iron" ? answer += 1 : answer += 1);
+    }
+    
+    if (nowPick === "stone") {
+      nowMinerals.forEach(item => item === "diamond" ? answer += 25 : item === "iron" ? answer += 5 : answer += 1);
+    }
+  }
+  
+  return answer;
 }
 
 solution([0, 1, 1], ["diamond", "diamond", "diamond", "diamond", "diamond", "iron", "iron", "iron", "iron", "iron", "diamond"]);
